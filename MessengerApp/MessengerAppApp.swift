f549cbb22e0d90439388d8334d304e8a583e1c94
Jin <m.davit@live.com>
2023-11-17 20:56:13 +0400

    Initial Commit

---
+++ MessengerApp/MessengerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerAppApp.swift
+//  MessengerApp
+//
+//  Created by Davit Margaryan on 17.11.23.
+//
+
+import SwiftUI
+
+@main
+struct MessengerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
